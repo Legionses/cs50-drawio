ca1b77e2f4f1e5754a52f9e569f18fed0d0fc632
Pavlo Mishchenko <pavlo@avatour.live>
2023-01-05 13:48:10 -0500

    adds painting on canvas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './styles/App.css';
-import Canvas from "./components/Canvas";
-import Header from "./components/Header";
+import React from 'react'
+import './styles/App.css'
+import Canvas from './components/Canvas'
+import Header from './components/Header'
 
 function App() {
-
-  return (
-    <div className="App">
-      <Header/>
-      <section>
-          <Canvas/>
-      </section>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <section className="App-body">
+                <Canvas />
+            </section>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- client/src/components/Canvas/Canvas.tsx
+++ client/src/components/Canvas/Canvas.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { MouseEventHandler, useEffect, useRef } from 'react'
+import styles from './styles.module.css'
 
+const getMousePos = (
+    canvas: HTMLCanvasElement,
+    evt: React.MouseEvent<HTMLCanvasElement>
+) => {
+    const rect = canvas.getBoundingClientRect()
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+    }
+}
 const Canvas = () => {
+    const canvas = useRef<HTMLCanvasElement>(null)
+    const isDrawing = useRef(false)
+
+    useEffect(() => {
+        if (!canvas.current) return
+        canvas.current.width = canvas.current.clientWidth
+        canvas.current.height = canvas.current.clientHeight
+        const mouseUp = () => {
+            if (isDrawing.current) isDrawing.current = false
+        }
+
+        document.addEventListener('mouseup', mouseUp)
+        return () => {
+            document.removeEventListener('mouseup', mouseUp)
+        }
+    }, [])
+
+    const mouseDown: MouseEventHandler<HTMLCanvasElement> = (evt) => {
+        if (!canvas.current) return
+        isDrawing.current = true
+        // @ts-ignore
+        const ctx: CanvasRenderingContext2D = canvas.current.getContext('2d')
+        const currentPosition = getMousePos(canvas.current, evt)
+        ctx.moveTo(currentPosition.x, currentPosition.y)
+        ctx.beginPath()
+        ctx.lineWidth = 1
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = `rgba(14, 255, 255, 1)`
+    }
+
+    const mouseMove: MouseEventHandler<HTMLCanvasElement> = (evt) => {
+        if (!isDrawing.current || !canvas.current) return
+        const currentPosition = getMousePos(canvas.current, evt)
+        // @ts-ignore
+        const ctx: CanvasRenderingContext2D = canvas.current.getContext('2d')
+        ctx.lineTo(currentPosition.x, currentPosition.y)
+        ctx.stroke()
+        // store(currentPosition.x, currentPosition.y, currentSize, currentColor)
+    }
+
     return (
-        <div>
-            <canvas></canvas>
-        </div>
+        <canvas
+            ref={canvas}
+            className={styles.canvas}
+            onMouseDown={mouseDown}
+            onMouseMove={mouseMove}
+        />
     )
 }
 
